8adcebb987dd8f7701692ff4b0c90d72744493e4
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-07-18 15:28:16 -0300

    refactor(ui, css): general UI and CSS adjustments

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -3,24 +3,24 @@
     <div class="card relative h-[180px] w-full overflow-hidden border-2 hover:border-secondary group">
       <div class="flex flex-col items-start gap-2 p-2 h-full">
         <div class="flex flex-col gap-2">
-          <h4>
+          <h4 class="truncate max-w-sm">
             {{ project.name }}
           </h4>
-          <p class="text-sm text-muted-foreground line-clamp-3">
+          <p class="text-sm text-muted-foreground line-clamp-3 break-all mr-4 max-w-sm max-h-sm">
             {{ project.description || 'No description provided.' }}
           </p>
         </div>
 
         <div class="absolute bottom-4 left-4 flex flex-row items-center gap-1 text-muted-foreground">
-          <Icon name="ph:key-bold" size="20" class="hover:text-primary hover:scale-md transition-all duration-500" />
-          <span class="font-semibold">{{ totalProjectSecrets }}</span>
+          <Icon name="ph:key-bold" size="20" class="group-hover:text-secondary group-hover:scale-sm transition-all duration-500" />
+          <span class="font-medium group-hover:text-secondary group-hover:scale-sm transition-all duration-500">{{ totalProjectSecrets }}</span>
 
-          <NuxtLink :to="`/admin/${project.id}/settings`" class="hover:text-accent hover:scale-md transition-all duration-500">
+          <NuxtLink :to="`/admin/${project.id}/settings`" class="group-hover:text-secondary group-hover:scale-sm transition-all duration-500">
             <Icon name="ph:gear-bold" size="20" class="mt-1 ml-2" />
           </NuxtLink>
         </div>
 
-        <Icon name="ph:arrow-right-bold" size="35" class="absolute bottom-4 right-4 text-muted-foreground group-hover:scale-md group-hover:text-accent transition-all duration-500" />
+        <Icon name="ph:arrow-right-bold" size="35" class="absolute bottom-4 right-4 text-muted-foreground group-hover:scale-sm group-hover:text-secondary transition-all duration-500" />
       </div>
     </div>
   </NuxtLink>
