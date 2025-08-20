7544b428b625f0cb7f8f04c27a09435b8b152748
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-08-20 09:46:20 -0300

    chore(deps): upgrade npm dependencies

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,19 +1,4 @@
 <template>
-  <header class="navigation-group justify-between border-b-2 bg-card p-4">
-    <nuxt-link to="/" class="hover:scale-sm flex flex-row items-center gap-2 transition-all">
-      <img
-        src="/assets/logo.png" alt="Logo"
-        width="40" height="40"
-        class="rounded-full"
-      >
-    </nuxt-link>
-
-    <nuxt-link to="/sign-in" class="btn">
-      <icon name="ph:sign-in-bold" size="20" />
-      <span class="hidden md:inline">Sign In</span>
-    </nuxt-link>
-  </header>
-
   <main class="bg-image min-h-screen overflow-hidden">
     <slot />
   </main>
